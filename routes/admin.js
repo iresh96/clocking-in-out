797181b09525a28bd3188081a2f3d6a6b365193f
DhanushaDevinda <devindarajapaksha12@gmail.com>
2022-10-19 14:15:23 +0530

    ui changes

--- routes/admin.js
+++ routes/admin.js
@@ -1,47 +1,58 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const connectEnsureLogin = require('connect-ensure-login');
+const connectEnsureLogin = require("connect-ensure-login");
 
-const Employee = require('../models/Employee')
-
+const Employee = require("../models/Employee");
 
 //Show add page
-router.get('/add',connectEnsureLogin.ensureLoggedIn('/auth/login'), (_req, res) => {
-    res.render('admin/add')
-  })
+router.get(
+  "/add",
+  connectEnsureLogin.ensureLoggedIn("/auth/login"),
+  (_req, res) => {
+    res.render("admin/add");
+  }
+);
 
 //Show admin Dashboard
-router.get('/',connectEnsureLogin.ensureLoggedIn('/auth/login'), async (_req, res) => {
-    res.render('admin')
-})
-
+router.get(
+  "/",
+  connectEnsureLogin.ensureLoggedIn("/auth/login"),
+  async (_req, res) => {
+    res.render("admin");
+  }
+);
 
 //POST Employee
-router.post('/',connectEnsureLogin.ensureLoggedIn('/auth/login'), async (req, res) => {
+router.post(
+  "/",
+  connectEnsureLogin.ensureLoggedIn("/auth/login"),
+  async (req, res) => {
     try {
-      await Employee.create(req.body)
-      res.redirect('/admin')
+      await Employee.create(req.body);
+      res.redirect("/admin");
     } catch (err) {
-      console.error(err)
-      res.render('errors/500')
+      console.error(err);
+      res.render("errors/500");
     }
-  })
-
-  // @desc    Show all Employees
-// @route   GET /employee
-router.get('/employees',connectEnsureLogin.ensureLoggedIn('/auth/login'),async (_req, res) => {
-  try {
-    const employees = await Employee.find()
-      .sort({ _id: 'asc' })
-      .lean()
-    res.render('admin/index', {
-      employees,
-    })
-  } catch (err) {
-    console.error(err)
-    res.render('errors/500')
   }
-})
+);
 
+// @desc    Show all Employees
+// @route   GET /employee
+router.get(
+  "/employees",
+  connectEnsureLogin.ensureLoggedIn("/auth/login"),
+  async (_req, res) => {
+    try {
+      const employees = await Employee.find().sort({ _id: "asc" }).lean();
+      res.render("admin/index", {
+        employees,
+      });
+    } catch (err) {
+      console.error(err);
+      res.render("errors/500");
+    }
+  }
+);
 
-module.exports = router
+module.exports = router;
